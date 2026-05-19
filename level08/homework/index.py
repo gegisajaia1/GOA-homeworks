3a9a8513458f4178ba3a143bb75d7612ff8c3cc1
gegisajaia1 <sajaiagegi@gmail.com>
2026-05-19 19:52:24 +0400

    second commit

---
+++ level08/homework/index.py
@@ -0,0 +1,30 @@
+print(True and False)
+print(False and False)
+print(False and True)
+print(False or False)
+print(5 > 10)
+
+print(10 > 4)
+print(9 > 2)
+print(214127546178 > 1)
+print(2 > 1)
+print(4 > 3)
+
+# sequancing არის სწორი წყობა როგორ კოდი არის დაწყობილი ისეთი თანმიმდევრობით გამოდის ტერმინალში შედეგი.
+# iteration-ის გამოყენებით შეგვიძლია სასურველი კოდი გავამეოროთ რამდენჯერაც გვინდა.
+# selection-ის მეშვეობით მომხმარებლის მიღებული არჩევნის შედეგად რა ინფორმაცია გადაწვდეს მომხმარებელს.
+# loop გვაძლევს შესაძლებლობას რომ არ მოგვიწიოს ერთი და იგივე კოდის ბევრჯერ დაწერა და მხოლოდ 2 ხაზის გამოყენებით შეგვიძლია მისი გაკეთება.
+# range ფუნქციას გადავცემთ ჩვენთვის სასურველ რიცხვს.
+
+for i in range(10):
+    print("mercedes")
+
+for i in range(100):
+    print("sajaia")
+
+for i in range(46):
+    print("black")
+
+for i in range(32):
+    print("G")
+    
