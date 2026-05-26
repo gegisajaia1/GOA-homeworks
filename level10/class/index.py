49ce23eae136dd2d7fd7329a0ef8abecae43b183
gegisajaia1 <sajaiagegi@gmail.com>
2026-05-26 20:22:19 +0400

    second commit

---
+++ level10/class/index.py
@@ -0,0 +1,17 @@
+number=1
+while  number < 11:
+     print( number)
+     number +=1
+
+
+even=2
+while even < 101:
+     print(even)
+     even += 2
+
+
+name=input("enter your name: ")
+if name == "petre":
+    print("გაუმარჯოს პეტრე")
+else:
+     print("შენ არ ხარ პეტრე")
---
+++ level10/home/index.py
@@ -0,0 +1,50 @@
+for i in range(39):
+  print(i)
+
+for i in range(30, 41):
+  print(i)
+
+  for i in range(12, 121):
+    print(i)
+
+for i in range(21):
+  print("gegi")
+
+for i in range(10, 101, 3): 
+  print(i)
+  
+
+for i in range(1, 31):
+    print(str(i) + " apple")
+
+
+
+age=input("enter your age: ")
+for i in range(13):
+   print(age)
+
+
+i = 1
+while i <= 10:
+    print("gegi")
+    i += 1
+
+
+i=1
+while i <= 24:
+   print(i)
+   i+=1
+
+
+i=1
+while i <= 200:
+   print(i)
+   i+=199
+
+
+age=15
+for i in range(0, 10):
+    print(age)
+
+for i in range(15):
+      print("sajaia")  
