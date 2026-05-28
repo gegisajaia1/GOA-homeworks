caf802359065bd7f3bef8d5a4742198aa33ef089
gegisajaia1 <sajaiagegi@gmail.com>
2026-05-28 23:35:28 +0400

    second commit

---
+++ level06/classwork/index.py
@@ -0,0 +1,22 @@
+age=input('enter your age: ')
+random_number=input('enter random number: ')
+
+print(age)
+print(random_number)
+print(int(age) + int(random_number))
+
+
+age2=input('enter your age: ')
+print(age2)
+print(int(age2) + 15)
+
+print(5>2)
+print(2<5)
+print(5>=5)
+print(5<=5)
+
+print(True and False)
+print(False and True)
+
+print(True or False)
+print(True or False)
