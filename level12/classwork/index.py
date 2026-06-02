2581a57209c8f6fa0af9e2b81adc35d77e53046b
gegisajaia1 <sajaiagegi@gmail.com>
2026-06-02 22:12:56 +0400

    second commit

---
+++ level12/classwork/index.py
@@ -0,0 +1,17 @@
+
+weather=input("enter your weather: ")
+if weather == "მზიანი":
+    print("ვივარჯიშებ გარეთ")
+elif weather == "მოღრუბლული":
+    print("ვივარჯიშებ გარეთ ოღონდ მოგვიანებით")
+else:
+    print("ასერთოდ არ ვივარჯიშებ")
+
+    for i in range(1, 1001):
+     if i == 461:
+        print("ნაპოვნია 461.")
+     break
+    for i in range(1, 100):
+           if i == 30:
+              print("ნაპოვნია ისეთი რიცხვი რომელიც იყოფა 2-ზეც 3-ზეც და 5-ზეც (30)")
+           break        
