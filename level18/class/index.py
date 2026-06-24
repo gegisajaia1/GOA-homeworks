bf7f84354c9aae6dea657897b2ce0c677aba6ae4
gegisajaia1 <sajaiagegi@gmail.com>
2026-06-24 16:35:57 +0400

    second commit

---
+++ level18/class/index.py
@@ -0,0 +1,28 @@
+#  ფუნქცია არის კოდის  ბლოკ რომელიც გარკვეულ ამოცანას ასრულებს
+# print() არის  built in ფუნქცია და მისი საქმეა ეკრანზე ინფორმაციის გამოტანა
+# არგუმენტი არის მნიშვნელობა რომელსაც  ფრჩხილებში ვწერთ
+# .upper() ყველა ასოს დიდად გადაიყვანს
+# .lower()  ყველა ასოს პატარად გადაიყვანს
+# .capitalize() მხოლოდ პირველ ასოს დიდად გადაიყვანს
+
+# color = "green"
+# print(color.upper())
+# dude = 'mud'
+# print(dude.upper())
+
+# color="yellow"
+# print(color.lower())
+# man='teacher'
+# print(man.lower())
+
+# city='tbilisi'
+# print(city.capitalize)
+# country='georgia'
+# print(country.capitalize)
+
+
+sentence = "Hello world!"
+print(sentence.find('world'))  
+print(sentence.find("mud"))   
+
+# .find() ეძებს სტრიქონში მოცემულ ტექსტს
---
+++ level18/home/index.py
@@ -0,0 +1,30 @@
+#ფუნქცია არის კოდი რომელიც ასრულებს კონკრეტულ დავალებას
+# print() არის  built in ფუნქცია და მას შეუძლია ეკრანზე ინფორმაციის გამოტანა
+# find() ტექსტში ეძებს  ტექსტს და აბრუნებს მის პოზიციას (ინდექსს). 
+
+sentence = "Hello world!"
+print(sentence.find('Hello'))  
+
+sentence = "i am 15 years old"
+print(sentence.find("name"))
+
+sentence = "my name is gegi"
+print(sentence.find("gegi"))
+
+ice="man"
+print(ice.upper())
+
+word='hello'
+print(word.upper())
+
+number="five"
+print(number.lower())
+
+string="lowercase"
+print(string.lower())
+
+dude='mud'
+print(dude.capitalize())
+
+country='georgia'
+print(country.capitalize())
